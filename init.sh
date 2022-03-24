a99f579de48a870bb39b0c3fbdcf76bf59154fa9
Tim <>
2022-03-24 16:51:56 +0100

    updates

--- init.sh
+++ init.sh
@@ -8,3 +8,9 @@ create extension postgis;
 EOSQL
 
 python3 scripts/changesetmd.py -d osmhistory -c -g -f 995.osm -p password -u dbuser
+
+
+
+#psql -U dbuser -d osmhistory <<EOSQL
+#update osm_changeset_state set last_sequence = {SEQUENCE};
+#EOSQL
